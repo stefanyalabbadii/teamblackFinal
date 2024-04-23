2dd7bfd2ffe4ac2429752f89ae2bdfcc72e814df
Newt <72360212+figgynwtn@users.noreply.github.com>
2024-04-23 13:18:24 -0400

    changed api to allow for maps

--- teamblackAPI/models/Restaurant.js
+++ teamblackAPI/models/Restaurant.js
@@ -7,17 +7,36 @@ const RestaurantSchema = new Schema({
         type: String,
         required: true
     },
-    price_level: {
-        type: Number,
-        required: true
-    },
-    rating: {
-        type: Number,
-        required: true
-    },
     city: {
         type: String,
         required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    cuisines: {
+        type: String
+    },
+    pickup_enabled: {
+        type: Boolean
+    },
+    delivery_enabled: {
+        type: Boolean
+    },
+    weighted_rating_value: {
+        type: Number
+    },
+    aggregated_rating_count: {
+        type: Number
     }
 });
 
