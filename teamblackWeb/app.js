a4f3e740c68d29f72d1bc963207e4fac60b8dd8f
EsmeraldaVelasco <143618680+EsmeraldaVelasco@users.noreply.github.com>
2024-04-29 16:36:40 -0400

    Cloud Function

--- teamblackWeb/app.js
+++ teamblackWeb/app.js
@@ -1,18 +1,14 @@
 // Import packages
 const express = require('express');
 const path = require('path');
-const {Storage} = require('@google-cloud/storage');
+
 
   // Set the port
   const port = 8080;
 
   // Create instances of necessary packages
   const app = express();
-  const storage=new Storage();
 
-
-  // Set the identifier for the GCS bucket where the favorite file will be stored
-  const bucket=storage.bucket('sp24-41200-teamblack-project-favorites');
 
 
   // Middleware
@@ -25,6 +21,8 @@ const {Storage} = require('@google-cloud/storage');
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+
 
 app.post('/results',(req, res) => {
   //Ensure that a city was entered
